d9c0a968342694e6a2ea5756b62503e35053f20c
agpavlik <agpavlik@gmail.com>
2023-05-04 23:50:03 +0000

    Modify functions getUserWithEmail, getUserWithID, addUser in database.js

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -1,47 +1,89 @@
 const properties = require("./json/properties.json");
 const users = require("./json/users.json");
 
-/// Users
+const { Pool } = require('pg');
 
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'lightbnb'
+});
+
+
+
+/// Users
+//-----------------------------------------------------------------
 /**
  * Get a single user from the database given their email.
  * @param {String} email The email of the user.
  * @return {Promise<{}>} A promise to the user.
  */
-const getUserWithEmail = function (email) {
-  let resolvedUser = null;
-  for (const userId in users) {
-    const user = users[userId];
-    if (user?.email.toLowerCase() === email?.toLowerCase()) {
-      resolvedUser = user;
-    }
-  }
-  return Promise.resolve(resolvedUser);
+
+const getUserWithEmail = (email) => {
+  pool
+    .query(
+      `SELECT * FROM users WHERE email = $1`, [email.toLowerCase()])
+    .then((result) => {
+      return result.rows[0];
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 
+// const getUserWithEmail = function (email) {
+//   let resolvedUser = null;
+//   for (const userId in users) {
+//     const user = users[userId];
+//     if (user.email.toLowerCase() === email.toLowerCase()) {
+//       resolvedUser = user;
+//     }
+//   }
+//   return Promise.resolve(resolvedUser);
+// };
+
+
+// ----------------------------------------------------------------
 /**
  * Get a single user from the database given their id.
  * @param {string} id The id of the user.
  * @return {Promise<{}>} A promise to the user.
  */
-const getUserWithId = function (id) {
-  return Promise.resolve(users[id]);
+
+const getUserWithId = (id) => {
+  pool
+    .query(
+      `SELECT * FROM users WHERE id = $1`, [id])
+    .then((result) => {
+      return result.rows[0];
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 
+// const getUserWithId = function (id) {
+//   return Promise.resolve(users[id]);
+// };
+
+// ----------------------------------------------------------------
 /**
  * Add a new user to the database.
  * @param {{name: string, password: string, email: string}} user
  * @return {Promise<{}>} A promise to the user.
  */
-const addUser = function (user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+const addUser = (user) => {
+  pool
+  .query(`INSERT INTO users (name, email, password) 
+    VALUES ($1, $2, $3) RETURNING *`, [user.name, user.email, user.password])
+  .then((result) => result.rows[0]) 
+  .catch((err) => console.log(err.message));
 };
 
-/// Reservations
 
+/// Reservations
+// ------------------------------------------------------------------
 /**
  * Get all reservations for a single user.
  * @param {string} guest_id The id of the user.
@@ -49,23 +91,43 @@ const addUser = function (user) {
  */
 const getAllReservations = function (guest_id, limit = 10) {
   return getAllProperties(null, 2);
+
 };
 
-/// Properties
 
+
+/// Properties
+//-------------------------------------------------------------------
 /**
  * Get all properties.
  * @param {{}} options An object containing query options.
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
-const getAllProperties = function (options, limit = 10) {
-  const limitedProperties = {};
-  for (let i = 1; i <= limit; i++) {
-    limitedProperties[i] = properties[i];
-  }
-  return Promise.resolve(limitedProperties);
+
+const getAllProperties = (options, limit = 10) => {
+  pool
+    .query(
+      `SELECT * FROM properties LIMIT $1`, [limit])
+    .then((result) => { 
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
+
+// const getAllProperties = function (options, limit = 10) {
+//   const limitedProperties = {};
+//   for (let i = 1; i <= limit; i++) {
+//     limitedProperties[i] = properties[i];
+//   }
+//   return Promise.resolve(limitedProperties);
+// };
+//-------------------------------------------------------------------
+
+
+
 
 /**
  * Add a property to the database
--- LightBnB_WebApp-master/server.js
+++ LightBnB_WebApp-master/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cookieSession = require("cookie-session");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 // middleware
 app.use(express.static(path.join(__dirname, "./public")));
